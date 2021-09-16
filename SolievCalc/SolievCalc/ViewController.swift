7843f3a0e35a982dc028cc16cb65e5bcf39dfd81
tishio <pureblack1337@gmail.com>
2021-09-16 14:09:10 +0300

    update calc(operand)

--- SolievCalc/SolievCalc/ViewController.swift
+++ SolievCalc/SolievCalc/ViewController.swift
@@ -12,6 +12,8 @@ class ViewController: UIViewController {
     var numberOne = ""
     var numberTwo = ""
     var onScreanSee = ""
+    var select = ""
+    var result = 0.0
     
     @IBOutlet weak var resoultLabel: UILabel!
     
@@ -32,35 +34,50 @@ class ViewController: UIViewController {
         numberOne = ""
         numberTwo = ""
         operand = ""
+        select = ""
         onScreanSee = "0"
+        result = 0.0
         resoultLabel.text = onScreanSee
     }
     
     @IBAction func inputOperand(_ sender: UIButton) {
         operand = (sender.titleLabel?.text)!
-        onScreanSee += operand
+        if onScreanSee == numberOne
+        {
+            onScreanSee += operand
+        }
+        else
+        {
+            onScreanSee = numberOne
+            onScreanSee += operand
+        }
         resoultLabel.text = onScreanSee
       }
     
     @IBAction func resultAction(_ sender: UIButton) {
-        var result = 0.0
-        switch operand {
-        case "+":
-            result = Double(numberOne)! + Double(numberTwo)!
-        case "/":
-            result = Double(numberOne)! / Double(numberTwo)!
-        case "-":
-            result = Double(numberOne)! - Double(numberTwo)!
-        case "*":
-            result = Double(numberOne)! * Double(numberTwo)!
-        default:
-            break
+            switch operand {
+            case "+":
+                result = Double(numberOne)! + Double(numberTwo)!
+            case "/":
+                result = Double(numberOne)! / Double(numberTwo)!
+            case "-":
+                result = Double(numberOne)! - Double(numberTwo)!
+            case "*":
+                result = Double(numberOne)! * Double(numberTwo)!
+            default:
+                break
+            }
+            if result.truncatingRemainder(dividingBy: 1.0) == 00 {
+                resoultLabel.text = String(Int(result))
+            } else {
+                resoultLabel.text = String(result)
+            }
+        if result != 0.0 {
+            numberOne = String(Int(result))
+            numberTwo = ""
+            operand = ""
+            select = ""
+            
         }
-        if result.truncatingRemainder(dividingBy: 1.0) == 00 {
-            resoultLabel.text = String(Int(result))
-        } else {
-            resoultLabel.text = String(result)
-        }
-        
     }
 }
